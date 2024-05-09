58f9dacb96c747e5a3bc8e6cc6fe206eac7ee5aa
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-05-09 16:58:49 +0900

    완료버튼 클릭 시 목록 취소선 및 글자색 회색변경 구현

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import styled from 'styled-components'
-import TodoListItem from './TodoListItem'
+import React from "react";
+import styled from "styled-components";
+import TodoListItem from "./TodoListItem";
 
-const TodoList = ({todoList, handleDelete}) => {
+const TodoList = ({ todoList, handleDelete, handleComplete }) => {
   return (
     <Box>
       <h1>할 일 목록</h1>
       {todoList.map((el) => {
-        return <TodoListItem handleDelete={handleDelete} key={el.id} id={el.id} body={el.body}></TodoListItem>
+        return (
+          <TodoListItem
+            complete={el.completed}
+            handleComplete={handleComplete}
+            handleDelete={handleDelete}
+            key={el.id}
+            id={el.id}
+            body={el.body}
+          ></TodoListItem>
+        );
       })}
     </Box>
-  )
-}
+  );
+};
 
 const Box = styled.div`
   margin-top: 50px;
@@ -19,7 +28,6 @@ const Box = styled.div`
   padding: 50px 20px;
   box-sizing: border-box;
   border-radius: 15px;
+`;
 
-`
-
-export default TodoList
+export default TodoList;
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-const TodoListItem = ({ id, body, handleComplete, handleDelete }) => {
+const TodoListItem = ({ id, body,complete, handleComplete, handleDelete }) => {
   return (
-    <Box>
+    <Box complete={complete}>
       <p>{body}</p>
       <div>
         <button
@@ -34,6 +34,10 @@ const Box = styled.div`
   border-radius: 5px;
   display: flex;
   justify-content: space-between;
+
+  //완료 버튼 눌렀을 때
+  text-decoration: ${props => props.complete ? "line-through" : "none"};
+  color:${props=>props.complete? "grey":"black"};
 `;
 
 export default TodoListItem;
